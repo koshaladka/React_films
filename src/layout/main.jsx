bc33ffcaf689728b624df6f2792601399c344f20
Alena home <79262812867@ya.ru>
2022-09-10 22:24:02 +0300

    all

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -13,16 +13,24 @@ class Main extends Component {
     }
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=6777d4e1&s=matrix`)
+        fetch(`https://www.omdbapi.com/?apikey=6777d4e1&s=matrix`)
             .then((response) => response.json())
             .then((data) => this.setState({movies: data.Search, loading: false}))
+            .catch((err) => {
+                console.error(err);
+                this.setState({loading: false})
+            })
     }
 
     searchMovies = (str, type = 'all') => {
         this.setState({loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=6777d4e1&s=${str}${type !== 'all' ?  `&type=${type}` : ''}`)
+        fetch(`https://www.omdbapi.com/?apikey=6777d4e1&s=${str}${type !== 'all' ?  `&type=${type}` : ''}`)
             .then((response) => response.json())
             .then((data) => this.setState({movies: data.Search, loading: false}))
+            .catch((err) => {
+                console.error(err);
+                this.setState({loading: false})
+            })
 
     }
 
